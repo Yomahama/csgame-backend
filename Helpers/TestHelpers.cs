ad6571532d1bd7cb4ec91a3932630ff21ed74757
Arnas Abromavicius <abromavicius.arnas@gmail.com>
2022-11-11 00:36:53 +0200

    prototype change

---
+++ Helpers/TestHelpers.cs
@@ -0,0 +1,22 @@
+﻿using csgame_backend.Data.Entities;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace csgame_backend.Helpers
+{
+    public class TestHelpers
+    {
+        Player? player;
+        Player? clone;
+        Player? deep_clone;
+        public void testPrototype()
+        {
+            player = new Player("Test", 264, -590);
+
+            clone = player.Clone();
+
+            deep_clone = player.DeepClone();
+            Console.WriteLine();
+        }
+    }
+}
